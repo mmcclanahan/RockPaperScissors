e7c66c21c7dc113708c921a2f1ade3fe5efb8f21
mmcclanahan <mmmcclan365@gmail.com>
2023-09-21 13:35:43 -0700

    added getPlayerChoice function checks if player input is valid

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const choices = ['rock', 'paper', 'scissors'];
+const options = ['rock', 'paper', 'scissors'];
 let wins = 0;
-let loss = 0;
+let losses = 0;
 
 function getComputerChoice() {
-    let choice = choices[Math.floor(Math.random() * choices.length)];
+    let choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
-function playRound(playerSelection, computerSelection) {
-    let c = computerSelection;
-    let p = playerSelection.toLowerCase();
+function getPlayerChoice() {
+    let playerChoiceValid = false;
+    let playerChoice = prompt('Rock, Paper, or Scissors?');
+    while (playerChoiceValid == false) {
+        if (playerChoice.toLowerCase() == 'rock' || playerChoice.toLowerCase() == 'paper' || playerChoice.toLowerCase() == 'scissors') {
+            playerChoiceValid = true;
+        }
+        else {
+            playerChoice = prompt('Error: Make sure choice is Rock, Paper, or Scissors.');
+        }
+    }
+    return playerChoice;
+}
+/*plays one round & prints the winner
+@param playerChoice prompt input of player choice
+@param computerChoice computers choice
+@return an integer 0 through 2 equating to outcome
+*/
+function playRound(playerChoice, computerChoice) {
+    let c = computerChoice;
+    let p = playerChoice.toLowerCase();
     if (p == 'rock' && c == 'scissors' || p == 'paper' && c == 'rock' || p == 'scissors' && c == 'paper') {
         console.log('You Win! ' + p + ' beats ' + c);
         return 1;
@@ -25,14 +43,18 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper, or Scissors?");
-        let computerSelection = getComputerChoice();
-        let whoWon = playRound(playerSelection, computerSelection);
-        if (whoWon == 1) wins++;
-        else if (whoWon == 2) loss++;
+        let playerChoice = getPlayerChoice();
+        let computerChoice = getComputerChoice();
+        let roundOutcome = playRound(playerChoice, computerChoice);
+        if (roundOutcome == 1) wins++;
+        else if (roundOutcome == 2) losses++;
         else continue;
     }
-    console.log('Final Score\nWins: ' + wins + '\nLosses: ' + loss);
+    if (wins > losses) console.log('You Win!');
+    else if (wins == losses) console.log('It\'s a Tie!');
+    else console.log('You Lost!');
+    console.log('Final Score\nWins: ' + wins + '\nLosses: ' + losses);
+    
 }
 game();
 
