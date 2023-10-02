f827e2b8a2428ce51dec53e2ca6efe14f77ef9a1
mmcclanahan <mmmcclan365@gmail.com>
2023-10-01 17:00:19 -0700

    JS added function to play game 5 times Allows player 5 tries then makes two p in div listing final score and a button

--- script.js
+++ script.js
@@ -8,44 +8,61 @@ function getComputerChoice() {
 }
 
 //get player choice
-
+const resultDiv = document.querySelector('#results')
+const resultDivv = document.querySelectorAll('#results')
 const btn = document.querySelectorAll('button');
-console.log(btn)
+
 btn.forEach(button => {
     button.addEventListener('click', () => {
         playRound(button.id);
     });
 });
+
 /*plays one round & prints the winner
-@param playerChoice prompt input of player choice
-@param computerChoice computers choice
+@param playerChoice button.id
 @return an integer 0 through 2 equating to outcome
 */
-const resultDiv = document.querySelector('#results')
 
+let count = 0;
 function playRound(buttonId) {
     
     let p = buttonId;
     let c = getComputerChoice();
+    if (count <= 4){    
+    count++;
     if (p == 'rock' && c == 'scissors' || p == 'paper' && c == 'rock' || p == 'scissors' && c == 'paper') {
-        
-        let pWin = document.createElement('p')
-        pWin.textContent = 'You Win! ' + p + ' beats ' + c;
-        resultDiv.appendChild(pWin);
-        return 1;
+            
+            let pWin = document.createElement('p')
+            pWin.textContent = 'You Win! ' + p + ' beats ' + c;
+            resultDiv.appendChild(pWin);
+            wins++;
+        }
+        else if (p == 'rock' && c == 'paper' || p == 'paper' && c == 'scissors' || p == 'scissors' && c == 'rock') {
+            let pLoss = document.createElement('p')
+            pLoss.textContent = 'You Lose! ' + c + ' beats ' + p;
+            resultDiv.appendChild(pLoss);
+            losses++;
+        }
+        else {
+            let pTie = document.createElement('p')
+            pTie.textContent = 'It\'s a tie!';
+            resultDiv.appendChild(pTie);
+        }
     }
-    else if (p == 'rock' && c == 'paper' || p == 'paper' && c == 'scissors' || p == 'scissors' && c == 'rock') {
-        let pLoss = document.createElement('p')
-        pLoss.textContent = 'You Lose! ' + c + ' beats ' + p;
-        resultDiv.appendChild(pLoss);
-        return 2;
-    }
-    else {
-        let pTie = document.createElement('p')
-        pTie.textContent = 'It\'s a tie!';
-        resultDiv.appendChild(pTie);
-        return 0;
-    }
+    else if (count == 5){
+        count++;
+        const playAgainBtn = document.createElement('button')
+        playAgainBtn.textContent = 'Play Again?'
+        let winOrLoss = document.createElement('p');
+        if(wins > losses) winOrLoss.textContent = 'Congratz! You Won!';
+        else if (losses > wins) winOrLoss.textContent = 'Maybe Next Time!';
+        else winOrLoss.textContent = 'A Tie!';
+        let finalScore = document.createElement('p');
+        finalScore.textContent = 'Final Score\nWins: ' + wins + '\nLosses: ' + losses;
+        resultDiv.appendChild(winOrLoss);
+        resultDiv.appendChild(finalScore);
+        resultDiv.appendChild(playAgainBtn);
+    }    
 }
 //while results div array is less than or equal to 4 keep playing
 //after the loop print the final score in the div and then clear the div
